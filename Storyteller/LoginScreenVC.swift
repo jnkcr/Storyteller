f08c63f635261f939675276bf7f9a2ecaad208f1
Jan Kučera <kucera.jan@hotmail.cz>
2022-03-04 23:14:34 +0100

    Login and signup - adjustments on screens - login logic added - signup logic added

--- Storyteller/LoginScreenVC.swift
+++ Storyteller/LoginScreenVC.swift
@@ -6,23 +6,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginScreenVC: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var invalidLoginLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log in"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        invalidLoginLabel.alpha = 0
     }
-    */
-
+    
+    @IBAction func loginAction(_ sender: Any) {
+        // Hide invalid label (it might have been triggered before)
+        invalidLoginLabel.alpha = 0
+        // Unwrap textfields
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        // Email validation
+        // ----- some code here ----
+        // Password validation
+        guard password.count > 6 else {
+            invalidLoginLabel.text = "Password: at least 6 characters"
+            invalidLoginLabel.alpha = 1
+            return
+        }
+        // Login user
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+            guard error == nil else {
+                guard let sSelf = self else { return }
+                sSelf.invalidLoginLabel.alpha = 1
+                print(error?.localizedDescription ?? "Login error")
+                return
+            }
+            print("User: \(result!.user.email!) (\(result!.user.uid))")
+        }
+    }
+    
 }
--- Storyteller/SignupScreenVC.swift
+++ Storyteller/SignupScreenVC.swift
@@ -6,23 +6,51 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignupScreenVC: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var password01TextField: UITextField!
+    @IBOutlet weak var password02TextField: UITextField!
+    @IBOutlet weak var invalidLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Sign up"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        invalidLabel.alpha = 0
     }
-    */
-
+    
+    @IBAction func signupAction(_ sender: Any) {
+        // Hide invalid label (it might have been triggered before)
+        invalidLabel.alpha = 0
+        // Unwrap textfields
+        guard let email = emailTextField.text else { return }
+        guard let password01 = password01TextField.text else { return }
+        guard let password02 = password02TextField.text else { return }
+        // Email validation
+        // ----- some code here ----
+        // Password validation
+        guard password01.count > 6 else {
+            invalidLabel.alpha = 1
+            return
+        }
+        guard password01 == password02 else {
+            invalidLabel.alpha = 1
+            return
+        }
+        // Create user
+        Auth.auth().createUser(withEmail: email, password: password01) { [weak self] result, error in
+            guard let sSelf = self else { return }
+            guard error == nil else {
+                sSelf.invalidLabel.alpha = 1
+                return
+            }
+        }
+    }
+    
 }
