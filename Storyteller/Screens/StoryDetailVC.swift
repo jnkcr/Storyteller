df91017d2e71990b1485c0e267b96f0631883781
Jan Kučera <kucera.jan@hotmail.cz>
2022-03-10 10:47:58 +0100

    Slider and stepper added

--- Storyteller/Screens/StoryDetailVC.swift
+++ Storyteller/Screens/StoryDetailVC.swift
@@ -15,14 +15,36 @@ class StoryDetailVC: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var gameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Story"
+        // Character description
         let character = storyDetailVM.getCharacter(named: name!)
         titleLabel.text = character.name
         gameLabel.text = character.game
         descriptionLabel.text = character.description
+        // Targets
+        stepper.addTarget(self, action: #selector(stepperValueChanged), for: .valueChanged)
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
+    }
+    
+}
+
+// MARK: - Target functions
+
+extension StoryDetailVC {
+    
+    @objc func stepperValueChanged() {
+        stepperLabel.text = "Step: \(Int(stepper.value))"
+    }
+    
+    @objc func sliderValueChanged() {
+        sliderLabel.text = "Value: " + String(format: "%.2f", slider.value)
     }
     
 }
